0e315e84e7b1b4e20b1bacea9ee30a2da0ba96d5
Ryan monesmith <ryan.monesmith@vtsd.com>
2022-10-28 07:45:01 -0400

    100 point

--- Temp.java
+++ Temp.java
@@ -1,2 +1,27 @@
-public class Temp {
+import java.awt.*;
+import java.applet.*;
+// g.drawLine(10,10,20,20);
+public class Temp extends Applet{
+    public void paint(Graphics g){
+
+
+        int x = 20;
+        int y = 20;
+        int width = 50;
+        int height = 50;
+        for(int k = 0; k < 50; k++){
+            g.drawOval(x,y,width,height);
+            x+=50;
+            y+=50;
+
+
+
+        }
+
+
+
+
+    }
+
+
 }
